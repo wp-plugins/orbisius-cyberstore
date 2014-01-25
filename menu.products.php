2d0c717e5cb90c0ed79b294be2e13d41c776b6c0
lordspace <lordspace@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-25 09:39:37 +0000

    tweaked manage products ui
    git-svn-id: https://plugins.svn.wordpress.org/orbisius-cyberstore/trunk@845144 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- menu.products.php
+++ menu.products.php
@@ -1,6 +1,5 @@
 <?php
-
-if (!current_user_can('manage_options') ) {
+if (!current_user_can('manage_options')) {
     wp_die('What?', 'Hi');
 }
 
@@ -15,7 +14,7 @@ $options = $orbisius_digishop_obj->get_options();
 $data = $orbisius_digishop_obj->get_products();
 
 $delete_url = $orbisius_digishop_obj->get('delete_product_url');
-$edit_url   = $orbisius_digishop_obj->get('edit_product_url');
+$edit_url = $orbisius_digishop_obj->get('edit_product_url');
 
 $adm_prefix = $orbisius_digishop_obj->get('plugin_url');
 $plugin_uploads_dir = $orbisius_digishop_obj->get('plugin_uploads_dir');
@@ -26,13 +25,12 @@ $active_product = " <img src='$adm_prefix/images/product_active.png' title='' al
 
 <div class="orbisius_cyberstore">
     <div class="wrap">
-        <h2>Products
-        <span class="op_cmd_right"><a href="<?php echo $orbisius_digishop_obj->get('plugin_admin_url_prefix') . '/menu.product.add.php';?>">Add Product</a></span>
-        </h2>
+        <h2>Products <a class="add-new-h2" href="<?php echo $orbisius_digishop_obj->get('plugin_admin_url_prefix') . '/menu.product.add.php'; ?>">Add Product</a></h2>
+        
         <p>The list of products you currently have. Copy the short code into the post where you'd like the buy now button to appear.</p>
 
         <div class="wrap" id="app-partners-container">
-            <table class="widefat fixed app_table_half">
+            <table class="widefat fixed app_table_half0 wp-list-table widefat fixed posts">
                 <thead>
                     <tr>
                         <th scope="col">Short Code</th>
@@ -49,41 +47,42 @@ $active_product = " <img src='$adm_prefix/images/product_active.png' title='' al
                         </tr>
                     <?php else: ?>
                         <?php foreach ($data as $idx => $rec) : ?>
-                        <tr>
-                            <td><?php echo "[" . $orbisius_digishop_obj->get('plugin_id_str') . " id=\"{$rec['id']}\"]"?>
+                            <tr>
+                                <td><?php echo "[" . $orbisius_digishop_obj->get('plugin_id_str') . " id=\"{$rec['id']}\"]" ?>
 
-                                <?php if (!empty($rec['file']) && !empty($rec['hash'])) : ?>
-                                    <small>
-                                        <a href="javascript:void(0);" onclick="jQuery('#download_link_container_<?php echo $rec['id']; ?>').toggle();">show/hide download link</a>
-                                        <div id="download_link_container_<?php echo $rec['id']; ?>" class="download_link app_hide">
-                                            <input type="text" value="<?php echo
-                                                Orbisius_CyberStoreUtil::add_url_params($orbisius_digishop_obj->get('site_url'),
-                                                        array($orbisius_digishop_obj->get('download_key') => $rec['hash']));?>" onclick="this.select();" />
-                                            <br/>click to select it and then right click and copy it.
-                                        </div>
-                                    </small>
-                                <?php endif; ?>
-                            </td>
-                            <td><?php echo $rec['label'];
+                                    <?php if (!empty($rec['file']) && !empty($rec['hash'])) : ?>
+                                        <small>
+                                            <a href="javascript:void(0);" onclick="jQuery('#download_link_container_<?php echo $rec['id']; ?>').toggle();">show/hide download link</a>
+                                            <div id="download_link_container_<?php echo $rec['id']; ?>" class="download_link app_hide widefat">
+                                                <input type="text" value="<?php
+                                                echo
+                                                Orbisius_CyberStoreUtil::add_url_params($orbisius_digishop_obj->get('site_url'), array($orbisius_digishop_obj->get('download_key') => $rec['hash']));
+                                                ?>" onclick="this.select();" />
+                                                <br/>click to select it and then right click and copy it.
+                                            </div>
+                                        </small>
+                                    <?php endif; ?>
+                                </td>
+                                <td><?php
+                                    echo $rec['label'];
 
-                            if (!empty($rec['file'])) {
-                                if (Orbisius_CyberStoreUtil::validate_url($rec['file']) || file_exists($plugin_uploads_dir . $rec['file'])) {
-                                    echo " <img src='$adm_prefix/images/attach.png' title='The product has a file linked to it.' alt='' />";
-                                } else {
-                                    echo " <img src='$adm_prefix/images/error.png' title='The product has a file linked to it but the file cannot be found.' alt='' />";
-                                }
-                            }
-                            
-                            ?></td>
-                            <td><?php echo $rec['price']?></td>
-                            <td>
-                                <a class="app_edit_button" href="<?php echo Orbisius_CyberStoreUtil::add_url_params($edit_url, array('id' => $rec['id']));?>">Edit</a>
-        |
-                                <a class="app_delete_button" onclick="return confirm('Are you sure?');"
-                                   href="<?php echo Orbisius_CyberStoreUtil::add_url_params($delete_url, array('id' => $rec['id']));?>">Delete</a>
-                            </td>
-                            <td><?php echo empty($rec['active']) ? $inactive_product : $active_product;?></td>
-                        </tr>
+                                    if (!empty($rec['file'])) {
+                                        if (Orbisius_CyberStoreUtil::validate_url($rec['file']) || file_exists($plugin_uploads_dir . $rec['file'])) {
+                                            echo " <img src='$adm_prefix/images/attach.png' title='The product has a file linked to it.' alt='' />";
+                                        } else {
+                                            echo " <img src='$adm_prefix/images/error.png' title='The product has a file linked to it but the file cannot be found.' alt='' />";
+                                        }
+                                    }
+                                    ?></td>
+                                <td><?php echo empty($rec['price']) ? 'Free' : $rec['price']; ?></td>
+                                <td>
+                                    <a class="app_edit_button" href="<?php echo Orbisius_CyberStoreUtil::add_url_params($edit_url, array('id' => $rec['id'])); ?>">Edit</a>
+                                    |
+                                    <a class="app_delete_button" onclick="return confirm('Are you sure?');"
+                                       href="<?php echo Orbisius_CyberStoreUtil::add_url_params($delete_url, array('id' => $rec['id'])); ?>">Delete</a>
+                                </td>
+                                <td><?php echo empty($rec['active']) ? $inactive_product : $active_product; ?></td>
+                            </tr>
                         <?php endforeach; ?>
                     <?php endif; ?>
                 </tbody>
