e157b73ec4a471586027309b70faa1337224c9aa
lordspace <lordspace@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-01 05:18:44 +0000

    removed the call to the bootstrap file
    git-svn-id: https://plugins.svn.wordpress.org/orbisius-cyberstore/trunk@849282 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -6,7 +6,6 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 
 $_ENV['ORBISIUS_DIGISHOP_TEST'] = 1;
 
-require_once(dirname(__FILE__) . '/orbisius-cyberstore.bootstrap.php');
 require_once(dirname(__FILE__) . '/orbisius-cyberstore.php');
 
 $orbisius_digishop_obj = Orbisius_CyberStore::get_instance();
